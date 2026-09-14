efdab1b143838052fb728623210b1a1820518038
BrianSabio <Brian232245@gmail.com>
2026-09-14 01:25:24 -0300

    feat: Se agrega ruta a profesionales

---
+++ routes/profesionalesRoutes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  obtenerProfesionales,
+  obtenerProfesionalPorId,
+  crearProfesional,
+  actualizarProfesional,
+  eliminarProfesional
+} = require("../controllers/profesionalesController");
+
+router.get("/", obtenerProfesionales);
+router.get("/:id", obtenerProfesionalPorId);
+router.post("/", crearProfesional);
+router.put("/:id", actualizarProfesional);
+router.delete("/:id", eliminarProfesional);
+
+module.exports = router;
