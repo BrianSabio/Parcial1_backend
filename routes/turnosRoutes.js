ba4831bab909466e4ef3c17fecf9306747190c04
BrianSabio <Brian232245@gmail.com>
2026-09-14 01:29:48 -0300

    feat: Se agrega ruta a turnos

---
+++ routes/turnosRoutes.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  obtenerTurnos,
+  obtenerTurnoPorId,
+  crearTurno,
+  actualizarTurno,
+  cancelarTurno,
+  atenderTurno,
+  eliminarTurno
+} = require("../controllers/turnosController");
+
+// GET / soporta query params ?profesionalId=, ?fecha=, ?pacienteId=, resueltos en el controller
+router.get("/", obtenerTurnos);
+router.get("/:id", obtenerTurnoPorId);
+router.post("/", crearTurno);
+router.put("/:id", actualizarTurno);
+router.patch("/:id/cancelar", cancelarTurno);
+router.patch("/:id/atender", atenderTurno);
+router.delete("/:id", eliminarTurno);
+
+module.exports = router;
