b4e6aa2030ef208bca7d8fc843a5209eb5a8bfaa
Pedro Bustamante <pmbusta@gmail.com>
2026-09-14 22:10:54 -0300

    feat: Reimplementar capa de modelo (Paciente y Profesional)

---
+++ models/Paciente.js
@@ -0,0 +1,12 @@
+class Paciente {
+  constructor(id, nombre, apellido, dni, telefono, email) {
+    this.id = id;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.dni = dni;
+    this.telefono = telefono;
+    this.email = email;
+  }
+}
+
+module.exports = Paciente;
---
+++ models/Profesional.js
@@ -0,0 +1,10 @@
+class Profesional {
+  constructor(id, nombre, apellido, especialidad) {
+    this.id = id;
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.especialidad = especialidad;
+  }
+}
+
+module.exports = Profesional;
